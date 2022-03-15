135766a7df1363a291e3f3ed10147ad8d53f654a
Александр Меркулов <merkulovwork@mail.ru>
2022-03-15 23:13:41 +1000

    laptopChange

--- src/app/services/blockone.service.ts
+++ src/app/services/blockone.service.ts
@@ -5,5 +5,42 @@ import { Injectable } from '@angular/core';
 })
 export class BlockoneService {
 
-  constructor() { }
+  constructor(public OneNature:number,
+    public OneTechnology:number,
+    public OneHuman:number,
+    public OneSign:number,
+    public OneArtistic:number,
+    
+    // test 2
+    public TwoBiolohy:number,
+    public TwoGeography:number,
+    public TwoGeology:number,
+    public TwoMedicine:number,
+    public TwoIndustry:number,
+    public TwoPhysics:number,
+    public TwoChemistry:number,
+    public TwoTechique:number,
+    public TwoEngineering:number,
+    public TwoMetalworking:number,
+    public TwoWoodworking:number,
+    public TwoConstruction:number,
+    public TwoTransport:number,
+    public TwoAAM:number,
+    public TwoMilitary:number,
+    public TwoHistory:number,
+    public TwoLiterature:number,
+    public TwoJournalism:number,
+    public TwoSocial:number,
+    public TwoPedagogy:number,
+    public TwoLaw:number,
+    public TwoService:number,
+    public TwoMaths:number,
+    public TwoEconomy:number,
+    public TwoForeign:number,
+    public TwoFigurative:number,
+    public TwoArt:number,
+    public TwoPerforming:number,
+    public TwoMusic:number,
+    public TwoSport:number,
+    ) { }
 }
