e015113f935c0f16e8fba653b6d7c2a0b213bb7b
Александр Меркулов <merkulovwork@mail.ru>
2022-04-17 13:38:01 +1000

    part of design saveFunc()

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { BlockoneService } from '../blockone.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   public data:any
-  public test:any
+  public test: BlockoneService  | undefined
+  // public test: BlockoneService = new BlockoneService(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+  
   constructor() { }
 }
